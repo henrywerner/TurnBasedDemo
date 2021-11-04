f188cb2831ee92580ef251cd45ede427025f1728
Henry W <htwerner01@gmail.com>
2021-11-03 19:16:40 -0500

    Update unity version + Add quit to menu

--- Assets/_Game/Scripts/GameState/GameStateWinBlue.cs
+++ Assets/_Game/Scripts/GameState/GameStateWinBlue.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStateWinBlue: IState
 {
     public void Enter()
     {
         Debug.Log("State Entered: Win Blue");
+        Debug.Log("<color=#fdbb43>PRESS [R] Restart; [M] Main menu</color>");
     }
 
     public void Tick()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(1);
+        else if (Input.GetKeyDown(KeyCode.M))
+            SceneManager.LoadScene(0);
     }
 
     public void FixedTick()
--- Assets/_Game/Scripts/GameState/GameStateWinRed.cs
+++ Assets/_Game/Scripts/GameState/GameStateWinRed.cs
@@ -1,14 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStateWinRed : IState
 {
     public void Enter()
     {
         Debug.Log("State Entered: Win Red");
+        Debug.Log("<color=#fdbb43>PRESS [R] Restart; [M] Main menu</color>");
     }
 
     public void Tick()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+            SceneManager.LoadScene(1);
+        else if (Input.GetKeyDown(KeyCode.M))
+            SceneManager.LoadScene(0);
     }
 
     public void FixedTick()
